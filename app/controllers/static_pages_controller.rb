7d750381335a62d0ebd3ba283b073c5662f1ec88
kaede-28 <kade28wa@gmail.com>
2024-10-28 11:50:21 +0000

    Finish 3

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,15 @@
 class StaticPagesController < ApplicationController
   def home
+    # render ....
+    # => app/veiws/static_pages/home.html.erb
   end
 
+    # => app/veiws/コントローラ名/アクション名.html.erb
   def help
+    # => app/veiws/static_pages/help.html.erb
+  end
+
+  def about
+    # => app/veiws/static_pages/about.html.erb
   end
 end
